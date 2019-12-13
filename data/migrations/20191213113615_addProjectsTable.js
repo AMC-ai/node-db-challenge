cdbb3c3dc38a36d9130ac725cff1e444d6c1069b
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-12-13 11:38:48 -0500

    project table added in migrate file

--- data/migrations/20191213113615_addProjectsTable.js
+++ data/migrations/20191213113615_addProjectsTable.js
@@ -1,8 +1,24 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return (
+        knex.schema
+            //projects table
+            .createTable("projects", tbl => {
+                //unq id
+                tbl.increments();
+
+                // name is required
+                tbl.string("name", 100)
+                    .notNullable();
+                //description
+                tbl.string("description", 100);
+                // boolean set to false 
+                tbl.boolean("completed").defaultTo(false);
+            })
+
+            
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+
 };
