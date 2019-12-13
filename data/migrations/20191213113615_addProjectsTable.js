00c34d56e97318f13f0ed33ad253ad7584665d7a
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-12-13 11:46:02 -0500

    added exports.down function for deletion in order of tables created in migrate folder

--- data/migrations/20191213113615_addProjectsTable.js
+++ data/migrations/20191213113615_addProjectsTable.js
@@ -75,5 +75,9 @@ exports.up = function (knex) {
 };
 
 exports.down = function (knex) {
-
+    return knex.schema
+        .dropTableIfExists('projects')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('project_resources');
 };
