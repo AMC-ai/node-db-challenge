0b79d130f67dcf715da83d01159ba83914643d69
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-12-13 12:31:49 -0500

    added getbyid for resource

--- resources/resource-router.js
+++ resources/resource-router.js
@@ -19,4 +19,26 @@ router.get('/', (req, res) => {
         });
 });
 
+//the same resource can be used in multiple projects.
+router.get("/:id", (req, res) => {
+    db.getById(req.params.id)
+        .then(resource => {
+            if (resource) {
+                res.status(200).json(resource);
+            } else {
+                res
+                    .status(404)
+                    .json({ message: 'The resource with the specified ID does not exist.' });
+            }
+        })
+        .catch(error => {
+            console.log('error on GET /resource/:id', error);
+            res
+                .status(500)
+                .json({
+                    message: 'The resource information could not be retrieved.',
+                });
+        });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const helmet = require("helmet");
 
 // const ProjectRouter = require("./projects/project-router");
-// const ResourceRouter = require("./resources/resource-router");
+const ResourceRouter = require("./resources/resource-router");
 
 const server = express();
 
 server.use(helmet(), express.json(), logger);
 
 // server.use("/api/projects", ProjectRouter);
-// server.use("/api/resources", ResourceRouter);
+server.use("/api/resources", ResourceRouter);
 
 //initial GET
 server.get('/', (req, res) => {
