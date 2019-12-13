6590e94239bdf34bd53da9e43abb0bf77c0e3b04
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-12-13 12:44:10 -0500

    added functions for project table in project-model

---
+++ projects/project-model.js
@@ -0,0 +1,28 @@
+const db = require("../data/db-config");
+
+module.exports = {
+    get,
+    getById,
+    insert,
+    // getTaskById,
+    // getTasks,
+    // addTask
+};
+
+function get() {
+    return db('projects');
+};
+
+function getById() {
+    return db('projects')
+        .where({ id })
+        .first();
+};
+
+function insert(project) {
+    return db('projects')
+        .insert(project, 'id')
+        .then(([id]) => {
+            return getById(id)
+        });
+};
